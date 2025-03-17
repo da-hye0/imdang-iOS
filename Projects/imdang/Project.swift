7223c06a6607c7419a37c092bba1760719775668
DAEJINLIM <daejin.limm@gmail.com>
2025-03-18 02:48:38 +0900

    Feat: version 1.0.1

--- Projects/imdang/Project.swift
+++ Projects/imdang/Project.swift
@@ -11,6 +11,8 @@ let project = Project(
             deploymentTargets: .iOS("15.0"),
             infoPlist: .extendingDefault(
                 with: [
+                    "CFBundleShortVersionString": "1.0.1",
+                    "CFBundleVersion": "1",
                     "UIUserInterfaceStyle": "Light",
                     "UILaunchStoryboardName": "LaunchScreen.storyboard",
                     "UIApplicationSceneManifest": [
